ebf3e28603a6e27546c1f5dd41305aa161f7cc35
SheikhULAB <tosibalaptop86@gmail.com>
2023-07-27 01:12:16 +0600

    created the header component

--- src/App.js
+++ src/App.js
@@ -6,11 +6,15 @@ import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import ForgotPassword from './pages/ForgotPassword';
 import Offers from './pages/Offers';
+import Header from './components/Header';
 
 function App() {
   return (
     <>
       <Router>
+
+        <Header />
+
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/profile' element={<Profile />} />
@@ -19,6 +23,7 @@ function App() {
           <Route path='/forgot-password' element={<ForgotPassword />} />
           <Route path='/offers' element={<Offers />} />
         </Routes>
+        
       </Router>
     </>
   );
